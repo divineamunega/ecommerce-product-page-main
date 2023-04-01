164ddc429c8adc3bf47d2901e0a3f8beb5975ca6
Divine Amunega <divineamunega@gmail.com>
2023-04-01 22:36:29 +0100

    modified:   index.html 	modified:   script.js 	modified:   scss/main.scss 	modified:   static/css/main.css 	modified:   static/css/main.css.map

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-const imgOpenModal = document.querySelector(".img-open-modal"); // Image that opens modal on click
-const modal = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".btn-close");
-const imgThumb = document.querySelectorAll(`.pic-thumbnail`);
-const imgThumbModal = document.querySelectorAll(`.pic-thumbnail-modal`);
-const picCont = document.querySelector(`.pic-cont`);
-const modalImg = document.querySelector(`.img-modal`);
-const whiteOverlay = document.querySelectorAll(".white-overlay");
+// const imgOpenModal = document.querySelector(".img-open-modal"); // Image that opens modal on click
+// const modal = document.querySelector(".overlay");
+// const btnCloseModal = document.querySelector(".btn-close");
+// const imgThumb = document.querySelectorAll(`.pic-thumbnail`);
+// const imgThumbModal = document.querySelectorAll(`.pic-thumbnail-modal`);
+// const picCont = document.querySelector(`.pic-cont`);
+// const modalImg = document.querySelector(`.img-modal`);
+// const overlayImg = document.querySelector(`.image-overlay`)
+// const whiteOverlay = document.querySelectorAll(".white-overlay");
 
-// Functions
-const removeWhiteOverlay = function () {
-  whiteOverlay.forEach((_, i) => whiteOverlay[i].classList.remove(`active`));
-};
-const changeImg = function (el) {
-  el.forEach((pic, i) => {
-    pic.addEventListener(`click`, () => {
-      modalImg.src = `images/image-product-${i + 1}.jpg`;
-      removeWhiteOverlay()
-      whiteOverlay[i].classList.add("active");
-    });
-  });
-};
+// // Functions
+// const removeWhiteOverlay = function () {
+//   whiteOverlay.forEach((_, i) => whiteOverlay[i].classList.remove(`active`));
+// };
+// const changeImg = function (el) {
+//   el.forEach((pic, i) => {
+//     pic.addEventListener(`click`, () => {
+//       modalImg.src = `images/image-product-${i + 1}.jpg`;
+//       removeWhiteOverlay()
+//       whiteOverlay[i].classList.add("active");
+//     });
+//   });
+// };
 
-modal.classList.add("hidden");
+// modal.classList.add("hidden");
 
-picCont.addEventListener("click", function () {
-  modal.classList.remove(`hidden`);
-});
-btnCloseModal.addEventListener(`click`, function () {
-  modal.classList.add(`hidden`);
-});
-modal.addEventListener(`click`, function () {
-  modal.classList.add(`hidden`);
-});
-// imgThumb.addEventListener(`click`, changeImgThumb);
-changeImg(imgThumb);
-changeImg(imgThumbModal);
+// picCont.addEventListener("click", function () {
+//   modal.classList.remove(`hidden`);
+// });
+// btnCloseModal.addEventListener(`click`, function () {
+//   modal.classList.add(`hidden`);
+// });
+// modal.addEventListener(`click`, function () {
+//   modal.classList.add(`hidden`);
+// });
+
+// changeImg(imgThumb);
+// changeImg(imgThumbModal);
