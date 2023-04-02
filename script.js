a66712bfdd5965222c2db956bd6747ac19f2b7f3
Divine Amunega <divineamunega@gmail.com>
2023-04-02 12:50:47 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -7,25 +7,28 @@ const imgThumbModal = document.querySelectorAll(`.pic-thumbnail-modal`);
 const picCont = document.querySelector(`.pic-cont`);
 const imgOverlay = document.querySelector(`.img-overlay`);
 const whiteOverlay = document.querySelectorAll(".white-overlay");
-const whiteOverlayModal = document.querySelectorAll('.white-overlay-modal');
-// const btnSliderNext = document,
+const whiteOverlayModal = document.querySelectorAll(".white-overlay-modal");
+const btnSliderNext = document.querySelector(".next");
+const btnSliderPrev = document.querySelector(".prev");
 
 // Functions
 const removeWhiteOverlay = function () {
   whiteOverlay.forEach((_, i) => whiteOverlay[i].classList.remove(`active`));
-  whiteOverlayModal.forEach((_, i) => whiteOverlayModal[i].classList.remove(`active`));
+  whiteOverlayModal.forEach((_, i) =>
+    whiteOverlayModal[i].classList.remove(`active`)
+  );
 };
-const changeImg = function (el) {
-  el.forEach((pic, i) => {
-    pic.addEventListener(`click`, () => {
-      imgModal.src = `images/image-product-${i + 1}.jpg`;
-      imgOverlay.src = `images/image-product-${i + 1}.jpg`;
-      removeWhiteOverlay();
-      whiteOverlay[i].classList.add("active");
-      whiteOverlayModal[i].classList.add("active");
-    });
-  });
+const changeImg = function () {
+  console.log(i);
+  imgModal.src = `images/image-product-${i + 1}.jpg`;
+  imgOverlay.src = `images/image-product-${i + 1}.jpg`;
+  removeWhiteOverlay();
+  whiteOverlay[i].classList.add("active");
+  whiteOverlayModal[i].classList.add("active");
 };
+imgThumb.forEach((pic) => {
+  pic.addEventListener(`click`, changeImg);
+});
 
 modal.classList.add("hidden");
 overlay.classList.add("hidden");
@@ -34,14 +37,44 @@ imgModal.addEventListener("click", function () {
   modal.classList.remove(`hidden`);
   overlay.classList.remove(`hidden`);
 });
+
 btnCloseModal.addEventListener(`click`, function () {
   modal.classList.add(`hidden`);
   overlay.classList.add(`hidden`);
 });
+
 overlay.addEventListener(`click`, function () {
   overlay.classList.add(`hidden`);
   modal.classList.add(`hidden`);
 });
 
-changeImg(imgThumb);
-changeImg(imgThumbModal);
+let i = 0;
+if (i > 0) {
+  i--;
+} else if (i === 0) {
+  i = 3;
+}
+
+btnSliderPrev.addEventListener(`click`, function () {
+  
+  imgModal.src = `images/image-product-${i + 1}.jpg`;
+  imgOverlay.src = `images/image-product-${i + 1}.jpg`;
+  removeWhiteOverlay();
+  whiteOverlay[i].classList.add("active");
+  whiteOverlayModal[i].classList.add("active");
+});
+
+btnSliderNext.addEventListener(`click`, function () {
+  if (i < 3) {
+    i++;
+  } else if (i === 3) {
+    i = 0;
+  }
+  imgModal.src = `images/image-product-${i + 1}.jpg`;
+  imgOverlay.src = `images/image-product-${i + 1}.jpg`;
+  removeWhiteOverlay();
+  whiteOverlay[i].classList.add("active");
+  whiteOverlayModal[i].classList.add("active");
+});
+// changeImg();
+// changeImg(imgThumbModal);
