2abec07f0ab4953d640f6cf5d3ea3e5614d31fc3
Divine Amunega <divineamunega@gmail.com>
2023-04-09 07:15:22 +0100

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -16,7 +16,9 @@ const number = document.querySelector(".number");
 const openCart = document.querySelector(`.cart-image`);
 const cart = document.querySelector(`.basket`);
 const addToCart = document.querySelector(`.add-to-cart-btn`);
-const cartContent = document.querySelector(`.cart-content`)
+const cartContent = document.querySelector(`.cart-content`);
+const checkOutBtn = document.querySelector(`.checkoutBtn`);
+const cartEmpty = document.querySelector(`.cart-empty`);
 
 cart.classList.add(`hidden`);
 
@@ -103,7 +105,7 @@ itemValueAdd.addEventListener("click", function () {
 });
 
 itemValueSubtract.addEventListener(`click`, function () {
-  cartValue > 0 ? cartValue-- : cartValue = 0;
+  cartValue > 0 ? cartValue-- : (cartValue = 0);
   number.textContent = cartValue;
 });
 
@@ -111,11 +113,43 @@ openCart.addEventListener(`click`, function () {
   cart.classList.toggle("hidden");
 });
 
-addToCart.addEventListener(`click`,function(){
-  cartContent.firstChild
-  console.log(cartContent.firstChild)
-  console.log(cartContent.appendChild)
-})
+checkOutBtn.classList.add(`hidden`);
+cartEmpty.classList.remove(`hidden`);
+addToCart.addEventListener(`click`, function () {
+  if (cartValue > 0) {
+    checkOutBtn.classList.remove(`hidden`);
+    cartContent.innerHTML = `<div class="cart-item">
+                <img
+                  src="${imgModal.src}"
+                  alt="..."
+                  class="pic" />
+                <div class="cart-info">
+                  <p class="cart-text">Fall Limited Edition Sneakers</p>
+                  <p  class="price-info d-flex"><span class="item-price" data-price="125.00">$125.00</span> <span class="item-value">X ${cartValue}</span> <span class="total-price">$${
+      125.0 * cartValue
+    }</span></p>
+                </div>
+                <button class="deleteBtn">
+                  <svg
+                    width="14"
+                    height="16"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <defs>
+                      <path
+                        d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z"
+                        id="a" />
+                    </defs>
+                    <use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a" />
+                  </svg>
+                </button>
+              </div>`;
+  }
+});
+
+
+
+
 
 /* const removeWhiteOverlay = function () {
 
